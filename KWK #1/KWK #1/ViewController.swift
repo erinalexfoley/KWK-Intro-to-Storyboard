d9b99ac28678bd276a3abe0420a9edd9cbfc124a
erinalexfoley <69254577+erinalexfoley@users.noreply.github.com>
2020-08-05 15:04:25 -0400

    Add files via upload
    it's alive!!!

--- KWK #1/KWK #1/ViewController.swift
+++ KWK #1/KWK #1/ViewController.swift
@@ -10,11 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var appTitle: UILabel!
+    
+    @IBOutlet weak var textfield: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
        print("This is a printing line. Testing, testing!")
     }
 
-
+    @IBAction func submit(_ sender: UIButton) {
+        if let newTitle = textfield.text {
+            appTitle.text = newTitle
+        }
+    }
+    
 }
 
