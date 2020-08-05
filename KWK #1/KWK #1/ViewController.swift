f299c7c976817e4f240f13b1ed48c489fef2beae
erinalexfoley <69254577+erinalexfoley@users.noreply.github.com>
2020-08-05 11:34:10 -0400

    Add files via upload

---
+++ KWK #1/KWK #1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KWK #1
+//
+//  Created by Erin Foley on 8/5/20.
+//  Copyright © 2020 Erin Foley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       print("This is a printing line. Testing, testing!")
+    }
+
+
+}
+
